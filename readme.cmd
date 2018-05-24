d15030a3cb0d124a3b0139f5781c18f71122e90e
Bourelle Frédéric <frederic.bourelle@gmail.com>
2018-05-24 15:43:31 +0200

    create readme.cmd

---
+++ readme.cmd
@@ -0,0 +1,23 @@
+#Symfony Movies Collection
+
+### Projet d'apprentissage de Symfony :
+Objectifs du module :
+- système d'enregistrement et d'authentification
+- Affichage d'une collection de films
+- Affichage des données de chaque film :
+  - Affiche
+  - Nom
+  - Réalisateurs, acteurs
+  - Durée
+  - Lien vers le site IMDB
+  - etc.
+- Création de reviews avec possibilité de laisser des commentaires et de supprimer des commentaires si on est connecté
+- Création d'une liste personnalisée pour les utilisateurs du système
+
+### Objectifs technique :
+- installation du projet
+- création d'entity, de model en lien avec la bdd
+- création d'utilisateurs et de commentaires factices avec faker
+- création et utilisation des formulaires
+- validation des données envoyées au serveur
+- mise en place d'une API simple (GET)
